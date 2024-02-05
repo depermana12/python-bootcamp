5cd154b71b0da580526c437039e24e8948d08bad
depermana12 <deddiapermana97@gmail.com>
2024-02-05 08:16:42 +0700

    created object oriented progamming

--- day16/day16.4.py
+++ day16/day16.4.py
@@ -1,15 +1,17 @@
-from menu import MenuItem, Menu
+from menu import Menu
 from money_machine import MoneyMachine
 from coffee_maker import CoffeeMaker
 
 operate = True
 
+# create object COFFEE_MAKER from class blueprint CoffeeMaker()
 coffee_maker = CoffeeMaker()
 menu = Menu()
 cashier = MoneyMachine()
 
 
 while operate:
+    # assigning object menu with get_items method
     options = menu.get_items()
     flavour = input("What would you like? (expresso/latte/cappuccino: ")
     if flavour == "off":
@@ -23,6 +25,3 @@ while operate:
         if coffee_maker.is_resource_sufficient(drink):
             if cashier.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
-
-
-
