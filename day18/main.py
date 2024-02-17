380341859eac818430ac4ae38f72e07d48eba8ca
depermana12 <deddiapermana97@gmail.com>
2024-02-18 00:19:14 +0700

    random turtle walk

--- day18/main.py
+++ day18/main.py
@@ -1,25 +1,27 @@
-from turtle import Turtle, Screen
+import turtle as t
+from random import choice, randint
 
-timmy = Turtle()
+timmy = t.Turtle()
+t.colormode(255)
+
 timmy.shape("turtle")
 timmy.color("purple", "aqua")
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
+
+timmy.pensize(5)
+timmy.speed(5)
+direction = [0, 90, 180, 270]
 
 
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+
+    selected_color = (r, g, b)
+    return selected_color
 
 
-
-
-
-
-
-
-
-
-
-
-screen = Screen()
-screen.exitonclick()
-
+for _ in range(200):
+    timmy.color(random_color())
+    timmy.forward(30)
+    timmy.setheading(choice(direction))
