0a51641061807b2fd372c0bb830666e056eb543d
depermana12 <deddiapermana97@gmail.com>
2024-02-21 17:28:28 +0700

    update main.py

--- day20-21/main.py
+++ day20-21/main.py
@@ -4,6 +4,11 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+TOP_WALL = 280
+BOTTOM_WALL = -280
+RIGHT_WALL = 280
+LEFT_WALL = -280
+
 snek = Snake()
 food = Food()
 scoreboard = Scoreboard()
@@ -22,10 +27,12 @@ screen.onkey(snek.down, "Down")
 screen.onkey(snek.left, "Left")
 screen.onkey(snek.right, "Right")
 
+
 while snek_running:
     screen.update()
     time.sleep(0.1)
     snek.move()
+    snek_head = snek.snek_body_seg[0]
 
     # Detect collisions against food
     if snek.snek_body_seg[0].distance(food) < 15:
@@ -34,7 +41,7 @@ while snek_running:
         scoreboard.scores()
 
     # Detect walls
-    if snek.snek_body_seg[0].xcor() > 280 or snek.snek_body_seg[0].xcor() < -280 or snek.snek_body_seg[0].ycor() > 280 or snek.snek_body_seg[0].ycor() < -280:
+    if snek_head.xcor() > RIGHT_WALL or snek_head.xcor() < LEFT_WALL or snek_head.ycor() > TOP_WALL or snek_head.ycor() < BOTTOM_WALL:
         snek_running = False
         scoreboard.game_over()
 
