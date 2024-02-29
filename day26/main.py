0d889aa788257f9706601e600f79a4e74bea149b
depermana12 <deddiapermana97@gmail.com>
2024-02-29 13:09:01 +0700

    update:error exception handling, recursive implementation

--- day26/main.py
+++ day26/main.py
@@ -5,7 +5,18 @@ raw_data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic = {row.letter: row.code for (index, row) in raw_data.iterrows()}
 print(nato_phonetic)
 
-user_input = input("Enter your name: ").upper()
 
-phonetic_result = [nato_phonetic.get(letter) for letter in user_input]
-print(phonetic_result)
+def recursive_phonetic():
+    user_input = input("Enter your name: ").upper()
+    try:
+        # if using get() method, the error return none
+        phonetic_result = [nato_phonetic[letter] for letter in user_input]
+    except KeyError:
+        print("Error, please input only a letter")
+        # invoke the function itself
+        recursive_phonetic()
+    else:
+        print(phonetic_result)
+
+
+recursive_phonetic()
